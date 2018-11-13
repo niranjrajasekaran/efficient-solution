ee13c2b7bc88766019fc47db606bd8c3f59314cf
Niranj Rajasekaran <niranjrajasekaran@localhost.localdomain>
2018-11-13 12:32:11 +0530

    IRCTC Seat Arrangment - Best Solution

---
+++ irctc_seat_arrangment.py
@@ -0,0 +1,42 @@
+from sys import stdin
+
+for eachcase in range(test_cases):
+    seat_number = int(stdin.readline())
+    qu = seat_number // 6
+    re = seat_number % 6
+    if re == 0:
+        if qu % 2 == 0:
+            print(seat_number - 11, end='')
+        else:
+            print(seat_number + 1, end='')
+        print(" WS")
+    elif re == 1:
+        if qu % 2 == 0:
+            print(seat_number + 11, end='')
+        else:
+            print(seat_number - 1, end='')
+        print(" WS")
+    elif re == 2:
+        if qu % 2 == 0:
+            print(seat_number + 9, end='')
+        else:
+            print(seat_number - 3, end='')
+        print(" MS")
+    elif re == 5:
+        if qu % 2 == 0:
+            print(seat_number + 3, end='')
+        else:
+            print(seat_number -9, end='')
+        print(" MS")
+    elif re == 3:
+        if qu % 2 == 0:
+            print(seat_number + 7, end='')
+        else:
+            print(seat_number - 5, end='')
+        print(" AS")
+    elif re == 4:
+        if qu % 2 == 0:
+            print(seat_number + 5, end='')
+        else:
+            print(seat_number - 7, end='')
+        print(" AS")
