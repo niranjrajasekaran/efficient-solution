002d56bae178bf80a1a0b18f1c1da19bacfe7b74
Niranj Rajasekaran <niranjrajasekaran@localhost.localdomain>
2018-11-13 12:34:20 +0530

    Removed comment codes - Added stdin in place of input() for good perfomance

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,6 @@
-"""
-input() - will be the input to which the 
-          range of prime numbers must be generated
-print(.., end=' ') - will generate the ouput in the same line
-"""
-for i in range(2, int(input())):
+from sys import stdin
+
+for i in range(2, int(stdin.readline())):
     flag = False
     for j in range(2, i):
         if (i % j) == 0:
